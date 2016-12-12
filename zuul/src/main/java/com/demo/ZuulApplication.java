5744913f385243b57b1ec2e1e64174a59029656c
Sridhar <Sridharb.in@gmail.com>
2016-12-12 10:34:42 +0530

    bvv

--- zuul/src/main/java/com/demo/ZuulApplication.java
+++ zuul/src/main/java/com/demo/ZuulApplication.java
@@ -2,15 +2,14 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@RefreshScope
 public class ZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
 	}
+	
 }
